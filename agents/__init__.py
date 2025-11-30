cb1b987214e17983e2a40f9bb3bbf0cc701c548c
Nitin <nitin.echo@gmail.com>
2025-11-30 19:12:02 +0530

    Fix CrewAI imports, add init files, update requirements, add runtime

---
+++ agents/__init__.py
@@ -0,0 +1,2 @@
+from .planner_agent import planner_agent
+from .researcher_agent import researcher_agent
---
+++ tasks/__init__.py
@@ -0,0 +1,2 @@
+from .research_task import research_task
+from .study_plan_task import study_plan_task
